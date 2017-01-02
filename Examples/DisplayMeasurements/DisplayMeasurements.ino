5908dd7b600da0bb00a969959f1db9d845949025
Arnd <Zanshin_Github@sv-zanshin.com>
2017-01-02 08:43:25 +0000

    Cleaned up example program

--- Examples/DisplayMeasurements/DisplayMeasurements.ino
+++ Examples/DisplayMeasurements/DisplayMeasurements.ino
@@ -1,9 +1,21 @@
 /*******************************************************************************************************************
 ** Example program for using the VCNL4010 library which allows the Arduino to use the VCNL4010 Fully Integrated   **
-** Proximity and Ambient Light Sensor with Infrared Emitter.                                                      **
+** Proximity and Ambient Light Sensor with Infrared Emitter. The most recent version of this program can be found **
+** at https://github.com/SV-Zanshin/VCNL4010/blob/master/Examples/DisplayMeasurements/DisplayMeasurements.ino     **
 **                                                                                                                **
 ** The VCNL4010 library uses the standard I2C library and assumes that the default hardware I2C ports for SDA and **
-** SCL are being utilized.                                                                                        **
+** SCL are being utilized. The address of the VCNL4010 is hardcoded at 0x13 and cannot be changed. The library is **
+** available on GitHub at https://github.com/SV-Zanshin/VCNL4010                                                  **
+**                                                                                                                **
+** This program sets up the VCNL4010 to sample the ambient light at 2x per second (which is the default) and to   **
+** average 128 measurements into each reading (which is more than the default 32 averages) to get more constant   **
+** measurements. The IR emitter is to the maximum value of 200mA (up from the default of 20mA) to extend the      **
+** range at which it detects movement and the sampling rate for proximity is raised to 125 from the default 2     **
+**                                                                                                                **
+** If either the ambient light or the movement sensor values change by more than 15% then the new values are      **
+** displayed to the serial port. Changes are detected using the abs() function, which turns out to be a macro in  **
+** the Arduino development environment and doesn't support embedded functions, which is why the checks are done   **
+** in separate commands rather than in one function call.                                                         **
 **                                                                                                                **
 ** This program is free software: you can redistribute it and/or modify it under the terms of the GNU General     **
 ** Public License as published by the Free Software Foundation, either version 3 of the License, or (at your      **
@@ -11,26 +23,28 @@
 ** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   **
 ** GNU General Public License for more details. You should have received a copy of the GNU General Public License **
 ** along with this program.  If not, see <http://www.gnu.org/licenses/>.                                          **
+**                                                                                                                **
+** Vers.  Date       Developer           Comments                                                                 **
+** ====== ========== =================== ======================================================================== **
+** 1.0.0  2017-01-02 Arnd@SV-Zanshin.Com Added PERCENTAGE constant and program description                        **
+**                                                                                                                **
 *******************************************************************************************************************/
 #include "VCNL4010.h"                                                         // Include the library              //
 /*******************************************************************************************************************
 ** Declare all program constants                                                                                  **
 *******************************************************************************************************************/
 const uint32_t SERIAL_SPEED = 9600;                                           // Set the baud rate for Serial I/O //
+const uint8_t  PERCENTAGE   = 15;                                             // Percentage delta trigger         //
 /*******************************************************************************************************************
 ** Declare global variables and instantiate classes                                                               **
 *******************************************************************************************************************/
-uint16_t ALS_Value       = 0;                                                 // Last displayed ALS reading       //
-uint16_t Proximity_Value = 0;                                                 // Last displayed Proximity reading //
-uint16_t ALS_Delta       = 0;                                                 // Difference between current & last//
-uint16_t Proximity_Delta = 0;                                                 // Difference between current & last//
 VCNL4010 Sensor;                                                              // Instantiate the class            //
 /*******************************************************************************************************************
 ** Method Setup(). This is an Arduino IDE method which is called upon boot or restart. It is only called one time **
 ** and then control goes to the main loop, which loop indefinately.                                               **
 *******************************************************************************************************************/
 void setup() {                                                                // Arduino standard setup method    //
-  Serial.begin(SERIAL_SPEED);                                                 // Start serial comms at set Baud   //
+  Serial.begin(SERIAL_SPEED);                                                 // Start serial comms at Baud rate  //
   delay(2000);                                                                // Some Atmel Serials need time     //
   Serial.println(F("Starting VCNL4010 display measurements program"));        //                                  //
   while (!Sensor.begin()) {                                                   // Loop until sensor found          //
@@ -39,21 +53,21 @@ void setup() {                                                                //
   } // of if-then we can't initialize or find the device                      //                                  //
   Sensor.setLEDmA(200);                                                       // Boost power to Proximity sensor  //
   Sensor.setAmbientLight(2,128);                                              // Sample 2x per second, 128 avgs.  //
-  Sensor.setProximityHz(64);                                                  // Sample 64x per second            //
+  Sensor.setProximityHz(128);                                                 // Sample 128x per second           //
   Serial.println(F("VCNL4010 initialized.\n\n"));                             //                                  //
 } // of method setup()                                                        //                                  //
 /*******************************************************************************************************************
 ** This is the main program for the Arduino IDE, it is an infinite loop and keeps on repeating.                   **
 *******************************************************************************************************************/
 void loop() {                                                                 //                                  //
-  uint16_t ALS                    = Sensor.getAmbientLight();                 // Get the ambient light value      //
-  uint16_t Proximity              = Sensor.getProximity();                    // Get the Proximity sensor value   //
-  if (ALS_Delta==0 && Proximity_Delta==0) {                                   // Set delta values in first loop   //
-    ALS_Delta       = ALS/10;                                                 // Set to 10% of value              //
-    Proximity_Delta = Proximity/10;                                           // Set to 10% of value              //
-  } // of if-then first loop                                                  //                                  //
-  int16_t ALS_Change       = ALS_Value - ALS;                                 // Compute the delta ALS            //
-  int16_t Proximity_Change = Proximity_Value - Proximity;                     // Compute the delta Proximity      //
+  static uint16_t ALS_Last        = 0;                                        // Last displayed ALS reading       //
+  static uint16_t Proximity_Last  = 0;                                        // Last displayed Proximity reading //
+  uint16_t ALS              = Sensor.getAmbientLight();                       // Get the ambient light value      //
+  uint16_t Proximity        = Sensor.getProximity();                          // Get the Proximity sensor value   //
+  uint16_t ALS_Delta        = Sensor.getAmbientLight()/PERCENTAGE;            // Set delta to percentage of value //
+  uint16_t Proximity_Delta  = Sensor.getProximity()/PERCENTAGE;               // Set delta to percentage of value //
+  int16_t  ALS_Change       = ALS_Last - ALS;                                 // Compute the delta ALS            //
+  int16_t  Proximity_Change = Proximity_Last - Proximity;                     // Compute the delta Proximity      //
   if (abs(ALS_Change)>ALS_Delta || abs(Proximity_Change)>Proximity_Delta) {   // If either is above threshold,    //
     Serial.print(F("Time = "));                                               // then display the new values      //
     Serial.print(millis()/1000);                                              // marking the ALS or proximity that//
@@ -62,9 +76,8 @@ void loop() {                                                                 //
     Serial.print(ALS);                                                        //                                  //
     Serial.print(F(", Proximity = "));                                        //                                  //
     if (abs(Proximity_Change)>Proximity_Delta) Serial.print(F("*"));          //                                  //
-    Serial.print(Proximity);                                                  //                                  //
-    Serial.println();                                                         //                                  //
-    ALS_Value       = ALS;                                                    //                                  //
-    Proximity_Value = Proximity;                                              //                                  //
+    Serial.println(Proximity);                                                //                                  //
+    ALS_Last       = ALS;                                                     //                                  //
+    Proximity_Last = Proximity;                                               //                                  //
   } // of if-then we have a 10% change or more in either reading              //                                  //
 } // of method loop()                                                         //----------------------------------//
