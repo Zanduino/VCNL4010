5fe97b033a62c5659c86e5cd5559841921c6322d
Doxygen CI <travis@travis-ci.org>
2020-12-27 08:33:48 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['begin',['begin',['../class_v_c_n_l4010.html#a99a63ea8fd42c97c35cf2cbcb5b547ff',1,'VCNL4010::begin(void)'],['../class_v_c_n_l4010.html#a6d6ddc9c6db8388a3c5ad0d956b02289',1,'VCNL4010::begin(const uint8_t deviceAddress)'],['../class_v_c_n_l4010.html#a6e7596deb949097bfce2fd77a46cf28e',1,'VCNL4010::begin(const uint32_t i2CSpeed)'],['../class_v_c_n_l4010.html#a6315d37d31db13a11dc7e8cb0af2c1f7',1,'VCNL4010::begin(const uint8_t deviceAddress, const uint32_t i2CSpeed)']]],
+  ['begin',['begin',['../class_v_c_n_l4010.html#a99a63ea8fd42c97c35cf2cbcb5b547ff',1,'VCNL4010::begin(void)'],['../class_v_c_n_l4010.html#a6d6ddc9c6db8388a3c5ad0d956b02289',1,'VCNL4010::begin(const uint8_t deviceAddress)'],['../class_v_c_n_l4010.html#aa94ce5ea42fb485c5a4d0a8b4ce40ce2',1,'VCNL4010::begin(const uint32_t &amp;i2CSpeed)'],['../class_v_c_n_l4010.html#a6b0a2604591692a6f772dec4275cb3aa',1,'VCNL4010::begin(const uint8_t deviceAddress, const uint32_t &amp;i2CSpeed)']]],
   ['bit_5fals_5fdata_5frdy',['BIT_ALS_DATA_RDY',['../_v_c_n_l4010_8h.html#aa617845c3bf9436df40c59a737f43366',1,'VCNL4010.h']]],
   ['bit_5fals_5fen',['BIT_ALS_EN',['../_v_c_n_l4010_8h.html#a18071484baa2a2b03465bfdc3ecf7176',1,'VCNL4010.h']]],
   ['bit_5fals_5fod',['BIT_ALS_OD',['../_v_c_n_l4010_8h.html#a6457f9d733a2041ce66d0f74ab2bde0a',1,'VCNL4010.h']]],
--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_v_c_n_l4010.html#a99a63ea8fd42c97c35cf2cbcb5b547ff',1,'VCNL4010::begin(void)'],['../class_v_c_n_l4010.html#a6d6ddc9c6db8388a3c5ad0d956b02289',1,'VCNL4010::begin(const uint8_t deviceAddress)'],['../class_v_c_n_l4010.html#a6e7596deb949097bfce2fd77a46cf28e',1,'VCNL4010::begin(const uint32_t i2CSpeed)'],['../class_v_c_n_l4010.html#a6315d37d31db13a11dc7e8cb0af2c1f7',1,'VCNL4010::begin(const uint8_t deviceAddress, const uint32_t i2CSpeed)']]]
+  ['begin',['begin',['../class_v_c_n_l4010.html#a99a63ea8fd42c97c35cf2cbcb5b547ff',1,'VCNL4010::begin(void)'],['../class_v_c_n_l4010.html#a6d6ddc9c6db8388a3c5ad0d956b02289',1,'VCNL4010::begin(const uint8_t deviceAddress)'],['../class_v_c_n_l4010.html#aa94ce5ea42fb485c5a4d0a8b4ce40ce2',1,'VCNL4010::begin(const uint32_t &amp;i2CSpeed)'],['../class_v_c_n_l4010.html#a6b0a2604591692a6f772dec4275cb3aa',1,'VCNL4010::begin(const uint8_t deviceAddress, const uint32_t &amp;i2CSpeed)']]]
 ];
